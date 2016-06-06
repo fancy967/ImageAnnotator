7eba873a77e4c782a49d98f38a84076f4a6f0b8f
Frankie Chan <5217wyx@gmail.com>
2016-06-06 16:28:48 +0800

    Sync wiht Object Marker.

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-var app = require('app');  // Module to control application life.
-var BrowserWindow = require('browser-window');  // Module to create native browser window.
+'use strict';
 
-// Report crashes to our server.
-require('crash-reporter').start();
+const electron = require('electron');
+const app = electron.app;  // Module to control application life.
+const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -13,22 +13,24 @@ app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   //if (process.platform != 'darwin') {
-  //    app.quit();
-  //}
   app.quit();
+  //}
 });
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on('ready', function () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1280, height: 700});
+  mainWindow = new BrowserWindow({
+    width: 1280, height: 700,
+    minWidth: 1280, minHeight: 700, autoHideMenuBar: true
+  });
 
   // and load the index.html of the app.
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
 
   // Open the DevTools.
-  mainWindow.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
